3d0e45bdb1774772f4c6fee5ac586e3a9bb0aa5a
ajagyabeng <ajagyabeng@gmail.com>
2023-04-12 15:00:16 +0000

    Register Login, Logout and Register URLs

--- todo_list/base/urls.py
+++ todo_list/base/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
+from django.contrib.auth.views import LogoutView
+
+
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView, CustomRegisterView
 
 urlpatterns = [
+    path("login/", CustomLoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(next_page="login"), name="logout"),
+    path("register/", CustomRegisterView.as_view(), name="register"),
+
     path("", TaskList.as_view(), name="tasks"),
     path("task/<int:pk>/", TaskDetail.as_view(), name="task"),
     path("task-create/", TaskCreate.as_view(), name="task-create"),
